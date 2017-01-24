7a037627aa66147109b03c00045bcafac778616d
lerina <icode.lerina@gmail.com>
2017-01-24 18:04:42 -0500

    Chap04: Variable Bindings

---
+++ 04.Variable_Bindings/02_Mutability.rs
@@ -0,0 +1,26 @@
+//[4.1 Mutability](http://rustbyexample.com/variable_bindings/mut.html)
+/*
+  Variable bindings are immutable by default,
+  but this can be overridden using the `mut` modifier.
+*/
+
+fn main() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error!
+    // _immutable_binding += 1;
+    // FIXME ^ Comment out this line
+}
+
+/*
+  The compiler will throw a detailed diagnostic about mutability errors
+*/
+
